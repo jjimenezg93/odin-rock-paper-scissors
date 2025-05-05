942173cd027cca1fa051c745016901f0006d8040
Julián Jiménez <jjimenezg93@gmail.com>
2025-05-05 16:01:52 +0100

    Add playRound + rest of game logic

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,32 +1,59 @@
 const kPlayOptions = ["Rock", "Paper", "Scissors"];
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let randIdx = Math.floor(Math.random() * 3);
   console.assert(
     randIdx >= 0 && randIdx < kPlayOptions.length,
     `Invalid random index: ${randIdx}`
   );
-  return kPlayOptions[randIdx];
+  return randIdx;
 }
 
-console.log("AI player's choice: " + getComputerChoice());
-
+// Step 5 requests that you let the user write the option
+// and use toLowerCase. I'm skipping that as I prefer to
+// use array + index.
 function getHumanChoice() {
   let message = "Choose an option";
   kPlayOptions.forEach((element, idx) => {
     message += `\n${idx}: ${element}`;
-});
-    const kRandOptionIdx = kPlayOptions.length;
-    message += `\n${kRandOptionIdx}: Random`;
+  });
+  const kRandOptionIdx = kPlayOptions.length;
+  message += `\n${kRandOptionIdx}: Random`;
 
   let userOption = parseInt(prompt(message));
   if (isNaN(userOption) || userOption < 0 || userOption > kRandOptionIdx) {
-    return "invalid";
+    return -1;
   }
   if (userOption == kRandOptionIdx) {
     return getComputerChoice();
   }
-  return kPlayOptions[userOption];
+  return userOption;
 }
 
-console.log("Human player's choice: " + getHumanChoice());
+function playRound(computerChoice, humanChoice) {
+  if (computerChoice === humanChoice) {
+    console.log("It's a draw");
+    return;
+  }
+  let bComputerWins = computerChoice === (humanChoice + 1) % 3;
+  if (bComputerWins) {
+    computerScore++;
+    console.log("AI player wins!");
+    return;
+  }
+  humanScore++;
+  console.log("Human player wins!");
+}
+
+let humanChoice = getHumanChoice();
+if (humanChoice === -1) {
+  console.log("Can't play the game because the human choice is invalid");
+} else {
+  console.log("Human player's choice: " + kPlayOptions[humanChoice]);
+  let computerChoice = getComputerChoice();
+  console.log("AI player's choice: " + kPlayOptions[computerChoice]);
+  playRound(computerChoice, humanChoice);
+}
