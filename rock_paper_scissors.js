a201e96be60d49e1e6bf948c606118a90b84fafd
Julián Jiménez <jjimenezg93@gmail.com>
2025-05-05 15:21:13 +0100

    Add getComputerChoice

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const kPlayOptions = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+  let randIdx = Math.floor(Math.random() * 3);
+  console.assert(
+    randIdx >= 0 && randIdx < kPlayOptions.length,
+    `Invalid random index: ${randIdx}`
+  );
+  return kPlayOptions[randIdx];
+}
+
+console.log(getComputerChoice());
