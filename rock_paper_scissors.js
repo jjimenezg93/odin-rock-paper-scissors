f61775a19c600157f1de802f69d96172484ebdbc
Julián Jiménez <jjimenezg93@gmail.com>
2025-05-05 15:38:48 +0100

    Add getHumanChoice

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -9,4 +9,24 @@ function getComputerChoice() {
   return kPlayOptions[randIdx];
 }
 
-console.log(getComputerChoice());
+console.log("AI player's choice: " + getComputerChoice());
+
+function getHumanChoice() {
+  let message = "Choose an option";
+  kPlayOptions.forEach((element, idx) => {
+    message += `\n${idx}: ${element}`;
+});
+    const kRandOptionIdx = kPlayOptions.length;
+    message += `\n${kRandOptionIdx}: Random`;
+
+  let userOption = parseInt(prompt(message));
+  if (isNaN(userOption) || userOption < 0 || userOption > kRandOptionIdx) {
+    return "invalid";
+  }
+  if (userOption == kRandOptionIdx) {
+    return getComputerChoice();
+  }
+  return kPlayOptions[userOption];
+}
+
+console.log("Human player's choice: " + getHumanChoice());
