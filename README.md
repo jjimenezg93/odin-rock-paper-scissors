4aad9281174269d421a6dbe642b691720d7ef270
Julián Jiménez <jjimenezg93@gmail.com>
2025-06-01 20:41:41 +0100

    Add basic GUI-based game functionality
    We dynamically build the play option buttons,
    enable/disable the buttons when appropriate, and
    allow for resetting the game

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
-# Rock-Paper-Scissors (console)
-
-Console version of the challenge
-
-# Landing Page
+# Rock-Paper-Scissors
 
 This is the solution to the [Rock-Paper-Scissors (console)][1] project of the JavaScript basics section in the [Foundations][2] course of The Odin Project.
 
 [1]: https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
 [2]: https://www.theodinproject.com/paths/foundations/courses/foundations#javascript-basics
+
+# Attributions
+
+Here you can find the list of attributions for each of the resources used:
+
+<img src="res/scissors.png" width=200px/>
+
+Image by <a href="https://pixabay.com/users/artsybeekids-392631/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5693652">Venita Oberholster</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5693652">Pixabay</a>
+
+<img src="res/rock.png" width=200px/>
+
+Image by <a href="https://pixabay.com/users/openclipart-vectors-30363/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=576667">OpenClipart-Vectors</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=576667">Pixabay</a>
+
+<img src="res/paper.png" width=200px/>
+
+Image by <a href="https://pixabay.com/users/openclipart-vectors-30363/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2024228">OpenClipart-Vectors</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2024228">Pixabay</a>
--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,5 +1,43 @@
 const kPlayOptions = ["Rock", "Paper", "Scissors"];
 
+let winnerScore = 5;
+
+let humanScore = 0;
+let computerScore = 0;
+
+let playerScoreElement = document.querySelector("p.player.score");
+let computerScoreElement = document.querySelector("p.computer.score");
+let resetBtnElement = document.querySelector("#btn-reset");
+
+let playerChoiceBtnsList = [];
+
+function onPlayerWins() {}
+
+function onComputerWins() {}
+
+function finishGame() {
+  resetBtnElement.classList.remove("disabled");
+  resetBtnElement.disabled = false;
+
+  playerChoiceBtnsList.forEach((element) => {
+    element.classList.add("disabled");
+    element.disabled = true;
+  });
+}
+
+function updateScore() {
+  playerScoreElement.textContent = humanScore;
+  computerScoreElement.textContent = computerScore;
+
+  if (humanScore == winnerScore) {
+    onPlayerWins();
+    finishGame();
+  } else if (computerScore == winnerScore) {
+    onComputerWins();
+    finishGame();
+  }
+}
+
 function getComputerChoice() {
   let randIdx = Math.floor(Math.random() * 3);
   console.assert(
@@ -31,25 +69,8 @@ function getHumanChoice() {
 }
 
 function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
   console.log("Results: Human vs AI");
 
-  function playRound(computerChoice, humanChoice) {
-    if (computerChoice === humanChoice) {
-      console.log("It's a draw");
-      return -1;
-    }
-    let bComputerWins = computerChoice === (humanChoice + 1) % 3;
-    if (bComputerWins) {
-      computerScore++;
-      console.log("AI player wins!");
-      return;
-    }
-    humanScore++;
-    console.log("Human player wins!");
-  }
   let currentRound = 0;
   while (currentRound < 5) {
     let humanChoice = getHumanChoice();
@@ -74,4 +95,77 @@ function playGame() {
   console.log(`Final score: ${humanScore} - ${computerScore}`);
 }
 
-playGame();
+function playRound(computerChoice, humanChoice) {
+  if (computerChoice === humanChoice) {
+    console.log("It's a draw");
+    return -1;
+  }
+  let bComputerWins = computerChoice === (humanChoice + 1) % 3;
+  if (bComputerWins) {
+    computerScore++;
+    console.log("AI player wins!");
+    return;
+  }
+  humanScore++;
+  console.log("Human player wins!");
+}
+
+function resetGame() {
+  resetBtnElement.classList.add("disabled");
+  resetBtnElement.disabled = true;
+
+  humanScore = 0;
+  computerScore = 0;
+  updateScore();
+
+  playerChoiceBtnsList.forEach((element) => {
+    element.classList.remove("disabled");
+    element.disabled = false;
+  });
+}
+
+function onPlayerSelectedChoice(choiceName) {
+  console.log(`Player selected ${choiceName}`);
+  playRound(getComputerChoice(), kPlayOptions.indexOf(choiceName));
+  updateScore();
+}
+
+function createChoice(parentNode, choiceName) {
+  console.assert(choiceName, `${choiceName} is an invalid choice`);
+  console.assert(
+    parentNode instanceof HTMLElement && parent !== undefined,
+    `createChoice called with invalid parentNode: ${parentNode}`
+  );
+
+  let choiceLowercase = choiceName.toLowerCase();
+
+  let listElementNode = document.createElement("li");
+  listElementNode.classList.add(choiceLowercase);
+
+  let btnNode = document.createElement("button");
+  let choiceIconNode = document.createElement("img");
+  choiceIconNode.src = `./res/${choiceLowercase}.png`;
+
+  btnNode.append(choiceIconNode);
+  if (parentNode.classList.contains("player")) {
+    playerChoiceBtnsList.push(btnNode);
+    btnNode.addEventListener(
+      "click",
+      onPlayerSelectedChoice.bind(null, choiceLowercase)
+    );
+  }
+
+  listElementNode.append(btnNode);
+  parentNode.append(listElementNode);
+}
+
+function createChoices(parentNode) {
+  kPlayOptions.forEach((choice) => createChoice(parentNode, choice));
+}
+
+let choiceListNodes = document.querySelectorAll("ul.choices");
+console.log(choiceListNodes);
+
+choiceListNodes.forEach((element, key, parent) => {
+  createChoices(element);
+});
